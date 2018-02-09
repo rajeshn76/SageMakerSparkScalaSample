b0301c3dc730aa497d274a53f5fd4c26a08fe58e
Rajesh Narasimhan <rajeshn76@gmail.com>
2018-02-08 19:28:25 -0500

    refactored configurations

--- src/main/scala/com/delegate/aws/sagemaker/KMeansMnist.scala
+++ src/main/scala/com/delegate/aws/sagemaker/KMeansMnist.scala
@@ -2,13 +2,15 @@ package com.delegate.aws.sagemaker
 
 import com.amazonaws.services.sagemaker.sparksdk.IAMRole
 import com.amazonaws.services.sagemaker.sparksdk.algorithms.KMeansSageMakerEstimator
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SparkSession
 
 object KMeansMnist extends App {
+  val conf = ConfigFactory.load()
 
   val spark = SparkSession.builder.master("local[*]").getOrCreate
-  spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", "AKIAI3RBG42G5OOCMPOQ")
-  spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", "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")
+  spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", conf.getString("aws.s3.accessKey"))
+  spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", conf.getString("aws.s3.secretAccessKey"))
 
   val trainingData = spark.read.format("libsvm")
     .option("numFeatures", "784")
@@ -18,10 +20,8 @@ object KMeansMnist extends App {
     .option("numFeatures", "784")
     .load("s3n://sagemaker-sample-data-us-east-1/spark/mnist/test")
 
-  val roleArn = "arn:aws:iam::130291900959:role/service-role/AmazonSageMaker-ExecutionRole-20180125T083005"
-
   val estimator = new KMeansSageMakerEstimator(
-    sagemakerRole = IAMRole(roleArn),
+    sagemakerRole = IAMRole(conf.getString("aws.roleArn")),
     trainingInstanceType = "ml.p2.xlarge",
     trainingInstanceCount = 1,
     endpointInstanceType = "ml.c4.xlarge",
